acc648e0066576e6412967caf0296c7e14e318bf
Peter Hyde-Smith <peter.k.hydesmith@gmail.com>
2019-07-26 11:40:21 -0500

    the correct classpath settings for derby are now in the weather.bat file and bash-setup.txt in the installation/ diretoctory. Also, the .bat file works for windows, if you create a shortcut that points to it.

--- src/main/java/io/javasmithy/data/Geodata.java
+++ src/main/java/io/javasmithy/data/Geodata.java
@@ -18,6 +18,7 @@ public class Geodata{
 
     public static void testDerby() {
         try {
+            Class.forName(driver);
             //DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
             conn = DriverManager.getConnection(connectionURL);
 
