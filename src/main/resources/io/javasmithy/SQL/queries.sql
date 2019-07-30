67722caae274d562923bb1c6556431d05ca97a62
Peter Hyde-Smith <peter.k.hydesmith@gmail.com>
2019-07-30 14:58:05 -0500

    sql query to find closest station to zip code completed

--- src/main/resources/io/javasmithy/SQL/queries.sql
+++ src/main/resources/io/javasmithy/SQL/queries.sql
@@ -38,12 +38,27 @@ CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE(
 );
 
 
-select *
+select min(sqrt((geodata.latitude-stations.latitude )*( geodata.latitude-stations.latitude)+( geodata.longitude-stations.longitude )*( geodata.longitude-stations.longitude ))) distance
 FROM GEODATA JOIN STATIONS
 on 
-    (geodata.latitude > (stations.latitude-.2) and geodata.latitude < (stations.latitude+.2))
+    (geodata.latitude > (stations.latitude-1) and geodata.latitude < (stations.latitude+1))
     and
-    (geodata.longitude > (stations.longitude-.2) and geodata.longitude < (stations.longitude+.2))
-WHERE geodata.zipcode = 53033 ;
+    (geodata.longitude > (stations.longitude-1) and geodata.longitude < (stations.longitude+1))
+WHERE geodata.zipcode = 53033 
+order by distance asc;
+
+
+##WHERE geodata.zipcode = 53027 min(sqrt( (( geodata.latitude-stations.latitude )( geodata.latitude-stations.latitude))+(( geodata.longitude-stations.longitude )( geodata.longitude-stations.longitude )) ));
+select * from geodata join stations
+on 
+WHERE geodata.zipcode = 53033 and  ;
+
+
+## -45.20362
+## -45.006318
+## -44.71004
+
+
+
 
 ##/c/Users/peter.hydesmith/Documents/projects/weather-projects/weather/src/main/resources/io/javasmithy/data/formatted/stations/stations.del
