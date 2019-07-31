2df1f3a69f4ebdba9840e607973c01f3bab84ccd
Peter Hyde-Smith <peter.k.hydesmith@gmail.com>
2019-07-31 10:26:37 -0500

    refactored some variable names within StationLocator and added comment to explain query. Also cleaned up formatting of query

--- src/main/java/io/javasmithy/data/StationLocator.java
+++ src/main/java/io/javasmithy/data/StationLocator.java
@@ -12,24 +12,25 @@ public class StationLocator{
 
     public static String findStation(String zipcode) {
         try {
+            //  This query uses the distance formula to find the shortest distance between zip code lat/long and stations within +/- 1 lat/long points of the zip code
             String findStationQuery = "select zipcode, geodata.latitude, geodata.longitude, station_id, stations.latitude, stations.longitude, "
-                                + "sqrt((geodata.latitude-stations.latitude )*( geodata.latitude-stations.latitude)"
-                                + "+( geodata.longitude-stations.longitude )*( geodata.longitude-stations.longitude )) distance "
-                                + "FROM GEODATA JOIN STATIONS on "
-                                + "(geodata.latitude > (stations.latitude-1) and geodata.latitude < (stations.latitude+1)) "
-                                + "and"
-                                + "(geodata.longitude > (stations.longitude-1) and geodata.longitude < (stations.longitude+1)) "
-                                + "where zipcode = " 
-                                + zipcode + " "
-                                + "order by distance asc fetch first row only";
+                                    + "sqrt((geodata.latitude-stations.latitude )*( geodata.latitude-stations.latitude)"
+                                    + "+( geodata.longitude-stations.longitude )*( geodata.longitude-stations.longitude )) distance "
+                                    + "FROM GEODATA JOIN STATIONS on "
+                                    + "(geodata.latitude > (stations.latitude-1) and geodata.latitude < (stations.latitude+1)) "
+                                    + "and"
+                                    + "(geodata.longitude > (stations.longitude-1) and geodata.longitude < (stations.longitude+1)) "
+                                    + "where zipcode = " 
+                                    + zipcode + " "
+                                    + "order by distance asc fetch first row only";
 
             Connection connection = DriverManager.getConnection(connectionURL);
             Statement statement = connection.createStatement();
             ResultSet results = statement.executeQuery(findStationQuery);
 
             results.next();
-            String station = results.getString(4);
-            return station;
+            String stationId = results.getString(4);
+            return stationId;
             
 
         } catch (Exception e) {
